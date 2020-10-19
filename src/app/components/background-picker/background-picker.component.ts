7205c98ad1ee0f0b421ce639cff50edf53ac6c9b
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2020-10-19 14:04:49 +0200

    Avanzo con background picker

--- src/app/components/background-picker/background-picker.component.ts
+++ src/app/components/background-picker/background-picker.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Background }                              from '../../model/background.model';
+import { BackgroundCategory }                      from '../../model/background-category.model';
+import { ApiService }                              from '../../services/api.service';
+import { CommonService }                           from '../../services/common.service';
+import { ClassMapperService }                      from '../../services/class-mapper.service';
+import { BackgroundInterface }                     from '../../interfaces/interfaces';
 
 @Component({
 	selector: 'game-background-picker',
@@ -6,6 +12,14 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./background-picker.component.scss']
 })
 export class BackgroundPickerComponent implements OnInit {
-	constructor() {}
+	show: boolean = false;
+	backgroundFilter: number = null;
+	backgroundList: Background[] = [];
+	backgroundListFiltered: Background[] = [];
+	selected: number = null;
+	
+	@Output() selectBackgroundEvent = new EventEmitter<BackgroundInterface>();
+
+	constructor(private as: ApiService, private cs: CommonService, private cms: ClassMapperService) {}
 	ngOnInit(): void {}
 }
