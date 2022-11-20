35d8f55b77802d0be1a34dd1485165403cbeeb6a
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-11-20 15:27:55 +0100

    Actualizo Angular CLI (14.2.9 -> 15.0.0), Angular (14.2.10 -> 15.0.0) y Angular CDK (14.2.7 -> 15.0.0). Función logout.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,32 @@
-import { BrowserModule }           from '@angular/platform-browser';
-import { NgModule }                from '@angular/core';
-import { DragDropModule }          from '@angular/cdk/drag-drop';
-import { AppRoutingModule }        from './app-routing.module';
-import { AppComponent }            from './app.component';
-import { FormsModule }             from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TokenInterceptor }        from './interceptors/token.interceptor';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { PAGES, COMPONENTS, PIPES, SERVICES }  from './app.common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { COMPONENTS, PAGES, PIPES, SERVICES } from './app.common';
+import { AppComponent } from './app.component';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		...PAGES,
-		...COMPONENTS,
-		...PIPES
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		HttpClientModule,
-		FormsModule,
-		BrowserAnimationsModule,
-    DragDropModule
-	],
-	providers: [
-		...SERVICES,
-		{
-			provide: HTTP_INTERCEPTORS,
-			useClass: TokenInterceptor,
-			multi: true
-		}
-	],
-	bootstrap: [AppComponent]
+  declarations: [AppComponent, ...PAGES, ...COMPONENTS, ...PIPES],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+  ],
+  providers: [
+    ...SERVICES,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/game/login/login.component.ts
+++ src/app/pages/game/login/login.component.ts
@@ -1,113 +1,113 @@
-import { Component, OnInit }       from '@angular/core';
-import { Router }                  from '@angular/router';
-import { LoginData, RegisterData } from '../../../interfaces/interfaces';
-import { ApiService }              from '../../../services/api.service';
-import { UserService }             from '../../../services/user.service';
-import { CommonService }           from '../../../services/common.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginData, RegisterData } from 'src/app/interfaces/interfaces';
+import { ApiService } from 'src/app/services/api.service';
+import { CommonService } from 'src/app/services/common.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
-	selector: 'game-login',
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.scss']
+  selector: 'game-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-	selectedTab: string = 'login';
-	loginData: LoginData = {
-		email: '',
-		pass: ''
-	};
-	registerData: RegisterData = {
-		email: '',
-		conf: '',
-		pass: ''
-	};
-	loading: boolean = false;
-	loginError: boolean = false;
-	registerError: string = null;
+  selectedTab: string = 'login';
+  loginData: LoginData = {
+    email: '',
+    pass: '',
+  };
+  registerData: RegisterData = {
+    email: '',
+    conf: '',
+    pass: '',
+  };
+  loading: boolean = false;
+  loginError: boolean = false;
+  registerError: string = null;
 
-	constructor(private as: ApiService,
-				private user: UserService,
-				private cs: CommonService,
-				private router: Router) {}
-	ngOnInit(): void {}
+  constructor(
+    private as: ApiService,
+    private user: UserService,
+    private cs: CommonService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {}
 
-	selectTab(option: string) {
-		if (this.loading) {
-			return;
-		}
-		this.selectedTab = option;
-	}
+  selectTab(option: string): void {
+    if (this.loading) {
+      return;
+    }
+    this.selectedTab = option;
+  }
 
-	checkLogin(ev: Event) {
-		ev && ev.preventDefault();
-		this.loginError = false;
+  checkLogin(ev: Event): void {
+    ev && ev.preventDefault();
+    this.loginError = false;
 
-		if (this.loginData.email=='') {
-			alert('¡No puedes dejar el email en blanco!');
-			return;
-		}
+    if (this.loginData.email == '') {
+      alert('¡No puedes dejar el email en blanco!');
+      return;
+    }
 
-		if (this.loginData.pass=='') {
-			alert('¡No puedes dejar la contraseña en blanco!');
-			return;
-		}
+    if (this.loginData.pass == '') {
+      alert('¡No puedes dejar la contraseña en blanco!');
+      return;
+    }
 
-		this.loading = true;
-		this.as.login(this.loginData).subscribe(result => {
-			if (result.status==='ok'){
-				this.user.logged = true;
-				this.user.id     = result.id;
-				this.user.email  = this.cs.urldecode(result.email);
-				this.user.token  = this.cs.urldecode(result.token);
-				this.user.saveLogin();
+    this.loading = true;
+    this.as.login(this.loginData).subscribe((result) => {
+      if (result.status === 'ok') {
+        this.user.logged = true;
+        this.user.id = result.id;
+        this.user.email = this.cs.urldecode(result.email);
+        this.user.token = this.cs.urldecode(result.token);
+        this.user.saveLogin();
 
-				this.router.navigate(['/game/hall']);
-			}
-			else{
-				this.loading = false;
-				this.loginError = true;
-			}
-		});
-	}
+        this.router.navigate(['/game/hall']);
+      } else {
+        this.loading = false;
+        this.loginError = true;
+      }
+    });
+  }
 
-	checkRegister(ev: Event) {
-		ev && ev.preventDefault();
-		this.registerError = null;
-		if (this.registerData.email=='') {
-			alert('¡No puedes dejar el email en blanco!');
-			return;
-		}
+  checkRegister(ev: Event): void {
+    ev && ev.preventDefault();
+    this.registerError = null;
+    if (this.registerData.email == '') {
+      alert('¡No puedes dejar el email en blanco!');
+      return;
+    }
 
-		if (this.registerData.conf=='') {
-			alert('¡No puedes dejar la confirmación del email en blanco!');
-			return;
-		}
+    if (this.registerData.conf == '') {
+      alert('¡No puedes dejar la confirmación del email en blanco!');
+      return;
+    }
 
-		if (this.registerData.email!==this.registerData.conf) {
-			alert('¡Los emails introducidos no coinciden!');
-			return;
-		}
+    if (this.registerData.email !== this.registerData.conf) {
+      alert('¡Los emails introducidos no coinciden!');
+      return;
+    }
 
-		if (this.registerData.pass=='') {
-			alert('¡No puedes dejar la contraseña en blanco!');
-			return;
-		}
+    if (this.registerData.pass == '') {
+      alert('¡No puedes dejar la contraseña en blanco!');
+      return;
+    }
 
-		this.loading = true;
-		this.as.register(this.registerData).subscribe(result => {
-			if (result.status==='ok'){
-				this.user.logged = true;
-				this.user.id     = result.id;
-				this.user.email  = this.cs.urldecode(result.email);
-				this.user.token  = this.cs.urldecode(result.token);
-				this.user.saveLogin();
+    this.loading = true;
+    this.as.register(this.registerData).subscribe((result) => {
+      if (result.status === 'ok') {
+        this.user.logged = true;
+        this.user.id = result.id;
+        this.user.email = this.cs.urldecode(result.email);
+        this.user.token = this.cs.urldecode(result.token);
+        this.user.saveLogin();
 
-				this.router.navigate(['/game/hall']);
-			}
-			else{
-				this.loading = false;
-				this.registerError = result.status;
-			}
-		});
-	}
+        this.router.navigate(['/game/hall']);
+      } else {
+        this.loading = false;
+        this.registerError = result.status;
+      }
+    });
+  }
 }
